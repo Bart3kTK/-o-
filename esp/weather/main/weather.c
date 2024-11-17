d7ecd472e8550713eef7d69426c225f2297d295f
AlaiveCYA <szymjar3@gmail.com>
2024-11-17 22:50:58 +0100

    [mering merging merging]

--- esp/weather/main/weather.c
+++ esp/weather/main/weather.c
@@ -6,18 +6,9 @@
 
 #define TAG "WEATHER_APP"
 
-<<<<<<< HEAD
 void app_main(void) {
 
   ESP_LOGI(TAG, "Weather app started.");
 
   app_task_create();
 }
-=======
-void app_main(void) { 
-    
-    ESP_LOGI(TAG, "Weather app started.");
-
-    app_task_create();
- }
->>>>>>> 3ffaa48e9cc5968c3b80abea8b2fb16b3aedf036

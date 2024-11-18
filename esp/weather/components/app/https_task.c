9ddd54403f2b435cdc6da3c81b187f6ed701b09d
Szymon Rzewuski <79797302+AlaiveCYA@users.noreply.github.com>
2024-11-18 13:37:39 +0100

    [Wifi and http]
    esp comunication init

---
+++ esp/weather/components/app/https_task.c
@@ -0,0 +1,104 @@
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+
+#define POST_REQUEST_DELAY_MS 20000
+
+#define POST_URL "http://localhost:8080/weather/masurements"
+
+#define TAG "HTTPS_TASK"
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+  switch (evt->event_id) {
+  case HTTP_EVENT_REDIRECT:
+    ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
+    break;
+  case HTTP_EVENT_ERROR:
+    ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+    break;
+  case HTTP_EVENT_ON_CONNECTED:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+    break;
+  case HTTP_EVENT_HEADER_SENT:
+    ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+    break;
+  case HTTP_EVENT_ON_HEADER:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key,
+             evt->header_value);
+    break;
+  case HTTP_EVENT_ON_DATA:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+    if (!esp_http_client_is_chunked_response(evt->client)) {
+      // Write out data
+      printf("%.*s", evt->data_len, (char *)evt->data);
+    }
+    break;
+  case HTTP_EVENT_ON_FINISH:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+    break;
+  case HTTP_EVENT_DISCONNECTED:
+    ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+    break;
+  }
+  return ESP_OK;
+}
+
+void generate_random_data(char *buffer, size_t buffer_size) {
+  float pressure = (float)(rand() % 100) / 10.0;
+  float temperature1 = (float)(rand() % 100) / 10.0;
+  float temperature2 = (float)(rand() % 100) / 10.0;
+  bool rainDetected = rand() % 2;
+  float humidity = (float)(rand() % 100) / 10.0;
+  float lightIntensity = (float)(rand() % 100) / 10.0;
+  float gasConcentration = (float)(rand() % 100) / 10.0;
+
+  snprintf(
+      buffer, buffer_size,
+      "{\n\t\"pressure\": %.1f, \"temperature1\": %.1f, \"temperature2\": "
+      "%.1f, "
+      "\"rainDetected\": %s, \"humidity\": %.1f, \"lightIntensity\": %.1f, "
+      "\"gasConcentration\": %.1f\n}",
+      pressure, temperature1, temperature2, rainDetected ? "true" : "false",
+      humidity, lightIntensity, gasConcentration);
+}
+
+void http_post_task(void *pvParameters) {
+
+  char post_data[256];
+
+  while (1) {
+
+    generate_random_data(post_data, sizeof(post_data));
+
+    esp_http_client_config_t config = {
+        .url = POST_URL,
+        .event_handler = _http_event_handler,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    esp_http_client_set_url(client, POST_URL);
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+      // ESP_LOGI(TAG, "HTTPS POST Status = %d, content_length = %d",
+      // esp_http_client_get_status_code(client),
+      // esp_http_client_get_content_length(client));
+    } else {
+      // ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+    }
+
+    esp_http_client_cleanup(client);
+
+    // Delay for 20 seconds
+    vTaskDelay(POST_REQUEST_DELAY_MS / portTICK_PERIOD_MS);
+  }
+}
+
+void http_post_task_create(void) {
+  xTaskCreate(&http_post_task, "https_post_task", 8192, NULL, 5, NULL);
+}

265e1512e14787e9b267ef0cb075b49a963e3e46
Bartek K <115420491+Bart3kTK@users.noreply.github.com>
2025-01-17 21:08:40 +0100

    Details (#18)
    * [details]
    
    small details upadte
    
    * [DTO update]
    
    added DTO
    created services
    
    * [alerts deploy]
    
    created endpoints to operate on alerts
    
    * [User&Alert/2]
    
    Created user endpoints
    Created half of alerts endpoints
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- spring/src/main/java/com/pogoda/weather/dto/EspMeasureUnitsDTO.java
+++ spring/src/main/java/com/pogoda/weather/dto/EspMeasureUnitsDTO.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EspUserDTO {
-    private String login;
-    private String password;
+public class EspMeasureUnitsDTO {
+    private String name;
 }

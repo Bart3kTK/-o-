c482b7e7a924e8148294b6e871b67b6a5a3ceb57
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2025-01-18 19:47:29 +0100

    added dht11 sensor lib

--- esp/weather/components/app/measures_task.c
+++ esp/weather/components/app/measures_task.c
@@ -1,1 +1,25 @@
+#define TAG "MEASURES_TASK"
 
+#include "measures_task.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "dht11.h"
+
+void measures_task(void *pvParameters) {
+    struct dht11_reading reading;
+
+    while(1) {
+        reading = DHT11_read();
+        if(reading.status == DHT11_OK) {
+            ESP_LOGI(TAG, "Temperature: %d, Humidity: %d", reading.temperature, reading.humidity);
+        } else {
+            ESP_LOGE(TAG, "Failed to read data from DHT11 sensor.");
+        }
+
+        
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+
+}
